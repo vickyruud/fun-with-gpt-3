ee4a0b60c8d1f80a00e3e0400a7823f118929a09
vickyruud <aprajit.k@gmail.com>
2022-05-11 22:55:38 -0400

    add material UI and mock data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import './styles/app.css'
+
+
+const mockData = {
+  text: 'Here goes poem',
+  index: 5,
+  logprobs: null,
+  finish_reason:'stop'
+}
 
 function App() {
 
@@ -6,30 +15,32 @@ function App() {
   
   //calls the api
   useEffect(() => {
-    const data = {
-      prompt: "Write a poem about a dog wearing skis",
-      temperature: 0.5,
-      max_tokens: 64,
-      top_p: 1.0,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-    };
 
-    fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
-      },
-      body: JSON.stringify(data),
-    }).then(res => res.json())
-      .then(data => {
-        console.log(data.choices[0])
-        setPoem(data.choices[0].text);
-      })
-      .catch(err => {
-      console.log(err);
-    })
+    setPoem(mockData.text);
+    // const data = {
+    //   prompt: "Write a poem about a dog wearing skis",
+    //   temperature: 0.5,
+    //   max_tokens: 64,
+    //   top_p: 1.0,
+    //   frequency_penalty: 0.0,
+    //   presence_penalty: 0.0,
+    // };
+
+    // fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+    //   },
+    //   body: JSON.stringify(data),
+    // }).then(res => res.json())
+    //   .then(data => {
+    //     console.log(data.choices[0])
+    //     setPoem(data.choices[0].text);
+    //   })
+    //   .catch(err => {
+    //   console.log(err);
+    // })
   }, [])
 
 
