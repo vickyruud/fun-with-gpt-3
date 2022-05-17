1890c60a05dd7a5fb4c2703d85b6987198770af6
vickyruud <aprajit.k@gmail.com>
2022-05-16 21:02:32 -0400

    fix navbar text spacing

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function NavBar() {
   return (
     <nav className="flex flex-row  py-2.5 items-center bg-white ">
-      <h1 className="self-center text-xl font-semibold  whitespace-nowrap dark:text-white p-2">Triv-AI- Ask AI anything</h1>
+      <h1 className="self-center text-xl font-semibold  whitespace-nowrap dark:text-white p-2">Triv-AI - Ask AI anything</h1>
     </nav>
   )
 }
