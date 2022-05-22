6ff7f4e1b968e42cc0635ad78999de08067bde02
vickyruud <aprajit.k@gmail.com>
2022-05-21 21:45:40 -0400

    remove accpet application json header tag

--- src/App.jsx
+++ src/App.jsx
@@ -73,8 +73,7 @@ function App() {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
-        Accept: "application/json" 
+        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
       },
       body: JSON.stringify(data),
     }).then(res => res.json())
